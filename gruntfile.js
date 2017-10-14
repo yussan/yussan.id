7302df4c466d6359344bdb45a0d3429803b3123e
yussan <yussandevelper@gmail.com>
2017-10-14 20:50:49 +0700

    feat(homepage): added introduction

---
+++ gruntfile.js
@@ -0,0 +1,145 @@
+module.exports = grunt => {
+  grunt.initConfig({
+    // pug task
+    pug: {
+      compile: {
+        options: {
+          data: {
+            debug: true
+          },
+          pretty: true
+        },
+        files: [{
+          src: '*.pug',
+          cwd: 'src/pug/',
+          dest: './',
+          expand: true,
+          ext: '.html'
+        }]
+      }
+    },
+
+    // sass task
+    sass: {
+      dist: {
+        options: {
+          style: 'inline'
+        },
+        files: [{
+          src: '*.sass',
+          cwd: 'src/sass/',
+          dest: 'dist/css',
+          expand: true,
+          ext: '.css'
+        }]
+      }
+    },
+
+    // uglify task (js minify)
+    uglify: {
+      my_target: {
+        files: {
+          'dist/js/app.min.js': ['src/js/app.js']
+        }
+      }
+    },
+
+    // copy task (copy src/libraries to dist/libraries)
+    copy: {
+      libraries: {
+        expand: true,
+        cwd: 'src',
+        src: ['libraries/**'],
+        dest: 'dist/'
+      },
+      toroot: {
+        expand: true,
+        cwd: 'src',
+        src: ['*.*'],
+        dest: './'
+      }
+    },
+
+    // image compress task (compress all image src/images to dist/images)
+    imagemin : {
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'src/',
+              src: ['images/**/*.{png,jpg,gif}'],
+              dest: 'dist'
+          }]
+      }
+    },
+    //minify css (only tun in production)
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css',
+          ext: '.css'
+        }]
+      }
+    },
+    // auto refresh view on change in dist directory
+    browserSync: {
+      dev: {
+        bsFiles: {
+            src : [
+                'dist/**.*',
+                '*.*'
+            ]
+        },
+        options: {
+            watchTask: true,
+            server: './'
+        }
+      }
+    },
+
+    // watch change inside directory to run task
+    watch: {
+      pug: {
+        files: ['src/pug/**/*.pug'],
+        tasks: ['pug']
+      },
+      sass: {
+        files: ['src/sass/**/*.sass'],
+        tasks: ['sass']
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['uglify']
+      },
+      copy: {
+        files: ['src/libraries/**', 'src/*.*'],
+        tasks: ['copy']
+      },
+      imagemin: {
+        files: ['src/images/**'],
+        tasks: ['imagemin']
+      }
+    }
+  });
+
+  // initial
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  //register default task
+  if(process.env.NODE_ENV == 'production')
+  {
+    grunt.registerTask('default', ['pug', 'sass', 'uglify', 'copy', 'imagemin', 'cssmin'])
+  }else 
+  {
+    grunt.registerTask('default', ['pug', 'sass', 'uglify', 'copy', 'imagemin', 'browserSync', 'watch'])
+  }
+};
